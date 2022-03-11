e119544957fc37ec85d8d5f55730b4d24eec319b
chidi udeogu <huxleyberg@LIVE.COM>
2022-03-11 22:34:51 +0100

    idv MemoryConfig added

---
+++ SecuringMicroservices/AuthServer/Config/MemoryConfig.cs
@@ -0,0 +1,25 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Config
+{
+    public class MemoryConfig
+    {
+        //this has to do with the identity of the user
+        public static IEnumerable<IdentityResource> IdentityResources() =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<Client> Clients() =>
+            new List<Client>
+            {
+
+            };
+    }
+}
