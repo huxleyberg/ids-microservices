62e1bce59ad0881301505d0734e500c3768bc715
chidi udeogu <huxleyberg@LIVE.COM>
2022-03-11 22:52:11 +0100

    testusers added

--- SecuringMicroservices/AuthServer/Config/MemoryConfig.cs
+++ SecuringMicroservices/AuthServer/Config/MemoryConfig.cs
@@ -1,7 +1,11 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AuthServer.Config
@@ -19,7 +23,40 @@ namespace AuthServer.Config
         public static IEnumerable<Client> Clients() =>
             new List<Client>
             {
+                new Client
+                {
+                    ClientId = "first-client",
+                    ClientSecrets = new []{new Secret("chidiSecret".ToSha512())},
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.OpenId}
 
+                }
+            };
+
+        public static IEnumerable<TestUser> TestUsers() =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "abcd1234",
+                    Username = "chidi",
+                    Password = "chidisPassword",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name","chidi"),
+                        new Claim("family_name", "xyz")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "abc2",
+                    Username = "Kiol",
+                    Password = "fghjk",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name","jpo"),
+                        new Claim("family_name", "yui")
+                    }
+                }
             };
     }
 }
