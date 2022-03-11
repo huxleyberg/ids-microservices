523bb1de97c1fab37714250ce2f39e89a655f354
chidi udeogu <huxleyberg@LIVE.COM>
2022-03-11 22:38:08 +0100

    configure memoryconfig in configureServices

--- SecuringMicroservices/AuthServer/Startup.cs
+++ SecuringMicroservices/AuthServer/Startup.cs
@@ -1,3 +1,4 @@
+using AuthServer.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,9 @@ namespace AuthServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer();
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(MemoryConfig.IdentityResources())
+                .AddInMemoryClients(MemoryConfig.Clients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
